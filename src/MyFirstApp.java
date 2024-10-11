1ea295dbea355c0938bd414b7a284aa022e93ee6
Enmanuel Almanzar Rosso <20231517@itla.edu.do>
2024-10-11 12:05:10 -0400

    First example of the book.

---
+++ src/MyFirstApp.java
@@ -0,0 +1,7 @@
+public class MyFirstApp {
+
+    public static void main(String[] args) {
+        System.out.println("Voy a ser de los mejores programadores");
+        System.out.println("del mundo");
+    }
+}
